f2f60ebb5edee2ab895fe952269cf21f25b9d939
Alireza Tajvidi <A.tajvidi.official@gmail.com>
2024-07-08 13:51:07 +0300

    Yanlisca dosyalar eklenmemis. Halledildi.

---
+++ Login page/Login page/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Login_page
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbKullanici = new System.Windows.Forms.Label();
+            this.picbxLogo = new System.Windows.Forms.PictureBox();
+            this.picbxKullanici = new System.Windows.Forms.PictureBox();
+            this.panKullanici = new System.Windows.Forms.Panel();
+            this.lbSifre = new System.Windows.Forms.Label();
+            this.picbxSifre = new System.Windows.Forms.PictureBox();
+            this.panSifre = new System.Windows.Forms.Panel();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxKullanici)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxSifre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbKullanici
+            // 
+            this.lbKullanici.AutoSize = true;
+            this.lbKullanici.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKullanici.Location = new System.Drawing.Point(108, 300);
+            this.lbKullanici.Name = "lbKullanici";
+            this.lbKullanici.Size = new System.Drawing.Size(101, 21);
+            this.lbKullanici.TabIndex = 0;
+            this.lbKullanici.Text = "Kullanıcı Adı:";
+            this.lbKullanici.UseMnemonic = false;
+            // 
+            // picbxLogo
+            // 
+            this.picbxLogo.Image = ((System.Drawing.Image)(resources.GetObject("picbxLogo.Image")));
+            this.picbxLogo.Location = new System.Drawing.Point(225, 74);
+            this.picbxLogo.Name = "picbxLogo";
+            this.picbxLogo.Size = new System.Drawing.Size(151, 154);
+            this.picbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbxLogo.TabIndex = 1;
+            this.picbxLogo.TabStop = false;
+            // 
+            // picbxKullanici
+            // 
+            this.picbxKullanici.Image = ((System.Drawing.Image)(resources.GetObject("picbxKullanici.Image")));
+            this.picbxKullanici.Location = new System.Drawing.Point(68, 276);
+            this.picbxKullanici.Name = "picbxKullanici";
+            this.picbxKullanici.Size = new System.Drawing.Size(45, 45);
+            this.picbxKullanici.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbxKullanici.TabIndex = 1;
+            this.picbxKullanici.TabStop = false;
+            // 
+            // panKullanici
+            // 
+            this.panKullanici.BackColor = System.Drawing.Color.SlateGray;
+            this.panKullanici.Location = new System.Drawing.Point(68, 327);
+            this.panKullanici.Name = "panKullanici";
+            this.panKullanici.Size = new System.Drawing.Size(448, 3);
+            this.panKullanici.TabIndex = 2;
+            // 
+            // lbSifre
+            // 
+            this.lbSifre.AutoSize = true;
+            this.lbSifre.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSifre.Location = new System.Drawing.Point(119, 414);
+            this.lbSifre.Name = "lbSifre";
+            this.lbSifre.Size = new System.Drawing.Size(45, 21);
+            this.lbSifre.TabIndex = 0;
+            this.lbSifre.Text = "Şifre:";
+            this.lbSifre.UseMnemonic = false;
+            // 
+            // picbxSifre
+            // 
+            this.picbxSifre.Image = ((System.Drawing.Image)(resources.GetObject("picbxSifre.Image")));
+            this.picbxSifre.Location = new System.Drawing.Point(68, 390);
+            this.picbxSifre.Name = "picbxSifre";
+            this.picbxSifre.Size = new System.Drawing.Size(45, 45);
+            this.picbxSifre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbxSifre.TabIndex = 1;
+            this.picbxSifre.TabStop = false;
+            // 
+            // panSifre
+            // 
+            this.panSifre.BackColor = System.Drawing.Color.SlateGray;
+            this.panSifre.Location = new System.Drawing.Point(68, 441);
+            this.panSifre.Name = "panSifre";
+            this.panSifre.Size = new System.Drawing.Size(448, 3);
+            this.panSifre.TabIndex = 2;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnGiris.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiris.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGiris.Location = new System.Drawing.Point(164, 496);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(275, 47);
+            this.btnGiris.TabIndex = 3;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(485, 410);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(253, 546);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifremi unuttum";
+            this.label1.UseMnemonic = false;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtSifre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSifre.Font = new System.Drawing.Font("Javanese Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSifre.Location = new System.Drawing.Point(164, 410);
+            this.txtSifre.Multiline = true;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(315, 25);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtKullanici.Font = new System.Drawing.Font("Javanese Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKullanici.Location = new System.Drawing.Point(215, 294);
+            this.txtKullanici.Multiline = true;
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(289, 25);
+            this.txtKullanici.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(594, 749);
+            this.Controls.Add(this.txtKullanici);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.panSifre);
+            this.Controls.Add(this.panKullanici);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picbxSifre);
+            this.Controls.Add(this.picbxKullanici);
+            this.Controls.Add(this.lbSifre);
+            this.Controls.Add(this.picbxLogo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbKullanici);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxKullanici)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxSifre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbKullanici;
+        private System.Windows.Forms.PictureBox picbxLogo;
+        private System.Windows.Forms.PictureBox picbxKullanici;
+        private System.Windows.Forms.Panel panKullanici;
+        private System.Windows.Forms.Label lbSifre;
+        private System.Windows.Forms.PictureBox picbxSifre;
+        private System.Windows.Forms.Panel panSifre;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullanici;
+    }
+}
+
